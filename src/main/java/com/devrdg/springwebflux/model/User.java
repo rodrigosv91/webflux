7030e4c233f9a85419c29ac644cc63e611a0a016
rodrigo <rodrigosv91@gmail.com>
2023-09-07 23:00:07 -0300

    add: cria classe user

---
+++ src/main/java/com/devrdg/springwebflux/model/User.java
@@ -0,0 +1,17 @@
+package com.devrdg.springwebflux.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private Long id;
+    private String name;
+}

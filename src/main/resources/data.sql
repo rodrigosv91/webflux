6168431976301fa8e6d6b8cf00693b8f780cddc7
rodrigo <rodrigosv91@gmail.com>
2023-09-11 22:55:18 -0300

    add: cria data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into  user(name) values('Joe1')
+insert into  user(name) values('Joe2')
+insert into  user(name) values('Joe3')
+insert into  user(name) values('Joe4')
+insert into  user(name) values('Joe5')
+

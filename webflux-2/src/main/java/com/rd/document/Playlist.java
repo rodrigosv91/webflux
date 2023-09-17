8c5115ad7ee57de2c0942d40727d3f1af7574592
rodrigo <rodrigosv91@gmail.com>
2023-09-17 00:16:57 -0300

    add: cria classe playlist

---
+++ webflux-2/src/main/java/com/rd/document/Playlist.java
@@ -0,0 +1,19 @@
+package com.rd.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Getter
+@Setter
+@AllArgsConstructor
+public class Playlist {
+
+    @Id
+    private String id;
+    private String nome;
+}

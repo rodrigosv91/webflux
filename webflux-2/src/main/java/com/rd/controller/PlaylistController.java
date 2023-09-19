014cc46b310e31d1644e6fc7ee8742f98e96a438
rodrigo <rodrigosv91@gmail.com>
2023-09-18 22:29:08 -0300

    add: cria playlist controller

---
+++ webflux-2/src/main/java/com/rd/controller/PlaylistController.java
@@ -0,0 +1,30 @@
+package com.rd.controller;
+
+import com.rd.document.Playlist;
+import com.rd.services.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PlaylistController {
+
+    @Autowired
+    PlaylistService playlistService;
+
+    @GetMapping(value = "/playlist" )
+    public Flux<Playlist> getPlaylist(){
+        return playlistService.findaAll();
+    }
+
+    @GetMapping(value = "/playlist/{id}")
+    public Mono<Playlist> getPlaylistById(@PathVariable String id){
+        return playlistService.findById(id);
+    }
+
+    @PostMapping(value = "/playlist")
+    public Mono<Playlist> save(@RequestBody Playlist playlist){
+        return playlistService.save(playlist);
+    }
+}

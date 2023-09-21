6a1c02df21342bac83300c852c0a31330ddbe1e3
rodrigo <rodrigosv91@gmail.com>
2023-09-20 22:32:41 -0300

    add: cria exemplo de eventos

--- webflux-2/src/main/java/com/rd/PlaylistRouter.java
+++ webflux-2/src/main/java/com/rd/PlaylistRouter.java
@@ -9,10 +9,10 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
-@Configuration
+//@Configuration
 public class PlaylistRouter {
 
-    @Bean
+    //@Bean
     public RouterFunction<ServerResponse> route(PlaylistHandler handler){
         return RouterFunctions
                 .route(GET("/playlist").and(accept(MediaType.APPLICATION_JSON)), handler::findAll)
--- webflux-2/src/main/java/com/rd/controller/PlaylistController.java
+++ webflux-2/src/main/java/com/rd/controller/PlaylistController.java
@@ -3,12 +3,16 @@ package com.rd.controller;
 import com.rd.document.Playlist;
 import com.rd.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//old rest handler
-//@RestController
+import java.time.Duration;
+
+//traditional rest handler
+@RestController
 public class PlaylistController {
 
     @Autowired
@@ -27,5 +31,14 @@ public class PlaylistController {
     @PostMapping(value = "/playlist")
     public Mono<Playlist> save(@RequestBody Playlist playlist){
         return playlistService.save(playlist);
+    }
+
+    @GetMapping(value = "/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents(){
+
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(5));
+        Flux<Playlist> events = playlistService.findaAll();
+        System.out.println("Evento Webflux");
+        return Flux.zip(interval, events);
     }
 }

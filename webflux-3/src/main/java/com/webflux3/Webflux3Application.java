5db168fa155cf1d048c47021f939e3e366468d1c
rodrigo <rodrigosv91@gmail.com>
2023-09-22 19:33:27 -0300

    add: cria projeto 3 webflux

---
+++ webflux-3/src/main/java/com/webflux3/Webflux3Application.java
@@ -0,0 +1,13 @@
+package com.webflux3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webflux3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webflux3Application.class, args);
+	}
+
+}

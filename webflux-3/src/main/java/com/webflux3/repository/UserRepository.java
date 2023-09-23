29ae077830d0e5b94fecab01911db46b6151c18f
rodrigo <rodrigosv91@gmail.com>
2023-09-23 20:25:31 -0300

    add: cria UserRepository

---
+++ webflux-3/src/main/java/com/webflux3/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.webflux3.repository;
+
+import com.webflux3.model.User;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+}

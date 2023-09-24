d6b92a0a837caa9f914eff71fd8193e62fab7d15
rodrigo <rodrigosv91@gmail.com>
2023-09-23 21:58:53 -0300

    add: cria UserService

---
+++ webflux-3/src/main/java/com/webflux3/service/UserService.java
@@ -0,0 +1,73 @@
+package com.webflux3.service;
+
+import com.webflux3.model.User;
+import com.webflux3.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
+    private final UserRepository userRepository;
+
+    public Mono<User> createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public Flux<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Mono<User> updateUser(String userId, User user){
+        return userRepository.findById(userId)
+                .flatMap(dbUser -> {
+                    dbUser.setAge(user.getAge());
+                    dbUser.setSalary(user.getSalary());
+                    return userRepository.save(dbUser);
+                });
+    }
+
+    public Mono<User> deleteUser(String idUser){
+        return userRepository.findById(idUser)
+                .flatMap(existingUser  -> userRepository.delete(existingUser)
+                        .then(Mono.just(existingUser))
+                );
+    }
+
+    public Mono<User> deleteUser2(String idUser){
+        return userRepository.findById(idUser)
+                .map(existingUser  -> {
+                    userRepository.delete(existingUser);
+                    return existingUser;
+                });
+    }
+
+    public Flux<User> fetchUsers(String name){
+        Query query = new Query()
+                .with(Sort
+                        .by(Collections.singletonList(Sort.Order.asc("age"))));
+
+        query.addCriteria(Criteria
+                .where("name")
+                .regex(name)
+        );
+
+        return reactiveMongoTemplate
+                .find(query,User.class);
+    }
+
+}

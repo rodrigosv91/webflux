e267d4bbd84f842ba721306b207c007f9a2e51c7
rodrigo <rodrigosv91@gmail.com>
2023-10-18 20:48:49 -0300

    initial commit

---
+++ webflux-4-essentials/src/main/java/org/example/webflux4essentials/Webflux4EssentialsApplication.java
@@ -0,0 +1,13 @@
+package org.example.webflux4essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webflux4EssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webflux4EssentialsApplication.class, args);
+	}
+
+}

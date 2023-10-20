dae630dad966cad416ffd2230ec7fad49ffab47b
rodrigo <rodrigosv91@gmail.com>
2023-10-19 23:01:13 -0300

    add: GlobalExceptionHandler

--- webflux-4-essentials/src/main/java/org/example/webflux4essentials/service/AnimeService.java
+++ webflux-4-essentials/src/main/java/org/example/webflux4essentials/service/AnimeService.java
@@ -23,8 +23,8 @@ public class AnimeService {
 
     public Mono<Anime> findById(int id){
         return animeRepository.findById(id)
-                .switchIfEmpty(monoResponseStatusNotFoundException())
-                .log();
+                .switchIfEmpty(monoResponseStatusNotFoundException());
+
     }
 
     public <T> Mono<T> monoResponseStatusNotFoundException(){
